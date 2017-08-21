1953da2bfef45072b6972f8c2ebf4b3e7d67e5f6
tangwei <tangwei@isec-tech.com>
2017-08-21 14:59:54 +0800

    Revert "test1 回滚"
    This reverts commit e24dab3137ff0e8570cc8be997a296fafd1e50c0.

--- swiftTest/swiftTest/ViewController.swift
+++ swiftTest/swiftTest/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        //test1 回滚
+        
     }
 
     override func didReceiveMemoryWarning() {
