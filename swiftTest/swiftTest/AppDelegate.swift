8421f9255dbac66b20add909d07fa5cb1a5aef51
tangwei <tangwei@isec-tech.com>
2017-08-21 15:05:03 +0800

    test 4 回滚

--- swiftTest/swiftTest/AppDelegate.swift
+++ swiftTest/swiftTest/AppDelegate.swift
@@ -22,6 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.rootViewController = tabBarController
         window?.makeKeyAndVisible()
         
+        //test 4 回滚
         
         return true
     }
